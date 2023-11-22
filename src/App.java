7f3eab65271f87eb8904b4b9241cad10edb10279
pablo-ramos-hernandez <usr2741@salesianos-lacuesta.net>
2023-11-22 10:06:09 +0000

    add(App):Creacion de todo el codigo de la clase main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import cliente.Cliente;
+import granjero.Granjero;
+import huerto.Huerto;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        /**
+         * LLamamos a todos los hilos y la clase huerto
+         */
+        Huerto huerto = new Huerto(5);
+
+        Granjero granjero = new Granjero("Manolo", huerto, 10);
+
+        Granjero granjero2 = new Granjero("Pablo", huerto, 5);
+
+        Cliente cliente = new Cliente("Pedro", huerto, 10);
+
+        Cliente cliente2 = new Cliente("Puchipon", huerto, 5);
+
+        granjero.start();
+        granjero2.start();
+        cliente.start();
+        cliente2.start();
     }
 }
