1923f6dc2684c38cc769c7f08d482a1f86fcd7f7
pablo-ramos-hernandez <usr2741@salesianos-lacuesta.net>
2023-11-22 09:53:15 +0000

    add(Granjero):Creacion del todo el codigo de la clase

---
+++ src/granjero/Granjero.java
@@ -0,0 +1,47 @@
+package Granjero;
+
+import java.util.Random;
+
+public class Granjero extends Thread {
+    private final String nombre;
+    private final Huerto huerto;
+    private final int cantidaVerduras;
+
+    /**
+     * Creamos el constructor y tenemos que implementar una clase Huerto para poder
+     * llamarlo sin necesidad de crear clases estaticas
+     */
+    public Granjero(String nombre, Huerto huerto, int cantidaVerduras) {
+        this.nombre = nombre;
+        this.huerto = huerto;
+        this.cantidaVerduras = cantidaVerduras;
+    }
+
+    /**
+     * Hacemos override al run donde le indicamos un bucle que pare cuando llegue
+     * hasta la cantidad de verduras y que intente hacer: Crear una variable donde
+     * va guardar la verdura aleatoria apartir del metodo getRadomVegetable, y que
+     * haga el metodo plantarVerdura
+     */
+    @Override
+    public void run() {
+        for (int i = 0; i < cantidaVerduras; i++) {
+            try {
+                String verdura = getRandomVegetable();
+                huerto.plantarVerdura(nombre, verdura);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Sirve para sacar un vegetal del array de forma aleatoria
+     */
+    private String getRandomVegetable() {
+        String[] verduras = { "lechuga", "papa", "apio", "espárragos", "rábano",
+                "brócoli", "alcachofa", "tomate", "pepino", "berenjena", "zanahoria" };
+
+        return verduras[new Random().nextInt(verduras.length)];
+    }
+}
